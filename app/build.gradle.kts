375475a65d11f68ce6af515a211349d4a8a453a7
goldshoot0720 <goldshoot0720@users.noreply.github.com>
2026-03-20 17:23:25 +0800

    Prepare v1.0.15 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.example.appwriteandroidtrae"
         minSdk = 24
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.0.14"
+        versionCode = 15
+        versionName = "1.0.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
