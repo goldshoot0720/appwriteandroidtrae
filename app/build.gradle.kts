f892dd84668aad867be603fa18d29ace0d6427e4
goldshoot0720 <goldshoot0720@users.noreply.github.com>
2026-04-03 00:30:00 +0800

    Bump version to 1.0.23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.example.appwriteandroidtrae"
         minSdk = 24
         targetSdk = 36
-        versionCode = 22
-        versionName = "1.0.22"
+        versionCode = 23
+        versionName = "1.0.23"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
